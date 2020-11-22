76e8b36b35144886a743dde84fbcb4831b4219fa
unknown <link49269@gmail.com>
2020-11-21 23:25:54 -0800

    First version of application

---
+++ models/genre.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: {
+    type: String,
+    // enum: [
+    //   "Action and Adventure",
+    //   "Comic Book",
+    //   "Mystery",
+    //   "Fantasy",
+    //   "Horror",
+    //   "Romance",
+    //   "Science Fiction",
+    //   "Historical Fiction",
+    //   "Biographies",
+    //   "Cookbooks",
+    // ],
+    minlength:3,
+    maxlength: 100,
+    required: true,
+
+  },
+});
+
+// Virtual for bookinstance's URL
+GenreSchema.virtual("url").get(function () {
+  return "/catalog/genre/" + this._id;
+});
+
+//Export model
+module.exports = mongoose.model("Genre", GenreSchema);
